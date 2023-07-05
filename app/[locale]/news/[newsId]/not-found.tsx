ac74b989941437902574e6b1f775c49dd9ec761e
rayvax <57082766+rayvax@users.noreply.github.com>
2023-07-05 13:04:55 +0300

    not found styles

--- app/[locale]/news/[newsId]/not-found.tsx
+++ app/[locale]/news/[newsId]/not-found.tsx
@@ -1,5 +1,17 @@
+import Link from 'next-intl/link';
+
 function NewsDetailsNotFound() {
-  return <div>News details not found</div>;
+  return (
+    <div className='news-details not-found'>
+      <h1>News not found</h1>
+      <p>
+        Go to{' '}
+        <Link href={'/'} className='home-link'>
+          News dashboard
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default NewsDetailsNotFound;
