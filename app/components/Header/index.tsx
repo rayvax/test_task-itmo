c20952714e08aa61ef08b633bb06b7a28833497b
rayvax <57082766+rayvax@users.noreply.github.com>
2023-07-05 13:20:50 +0300

    logo to assets

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -2,18 +2,14 @@ import Image from 'next/image';
 import { LanguageDropdown } from '../../[locale]/LanguageDropdown';
 import Link from 'next-intl/link';
 
+import ItmoLogo from './assets/logo.png';
+
 export function Header() {
   return (
     <header className='main-header'>
       <div className='main-content content-wrapper'>
         <Link href='/'>
-          <Image
-            src='/images/logo.png'
-            alt='ITMO logo'
-            width={161}
-            height={16}
-            className='main_logo'
-          />
+          <Image src={ItmoLogo} alt='ITMO logo' width={161} height={16} className='main_logo' />
         </Link>
         <div className='change-lang'>
           <LanguageDropdown />
